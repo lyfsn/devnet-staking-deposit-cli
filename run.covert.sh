33e2fdf1db2dd8785e92853a22844253e42a5e7f
lyfsn <dev.wangyu@proton.me>
2023-12-07 11:07:12 +0800

    up

---
+++ run.covert.sh
@@ -0,0 +1,8 @@
+./deposit.sh install  
+
+
+./deposit.sh generate-bls-to-execution-change 
+
+
+# marine fox season boat flee bid execute involve tree agree sick fault kind during quit monitor guard impulse half dawn drill drastic salon faith
+
--- run.new.sh
+++ run.new.sh
@@ -1,6 +1,14 @@
-./deposit.sh install  
+# ./deposit.sh install  
 
 
-./deposit.sh new-mnemonic    
+# ./deposit.sh new-mnemonic    
 
 # marine fox season boat flee bid execute involve tree agree sick fault kind during quit monitor guard impulse half dawn drill drastic salon faith
+
+
+
+./deposit.sh new-mnemonic --num_validators=1 --mnemonic_language=english --chain=holesky 
+
+
+# rebuild envelope bean express sphere luggage carry expose name voyage limit parade curtain walnut inquiry link crystal marriage define angle tennis then match cram
+# peasant loyal smile patrol absorb regret chunk abstract hospital steak almost glad student argue myth margin end under wire badge patch rifle warrior wrist
