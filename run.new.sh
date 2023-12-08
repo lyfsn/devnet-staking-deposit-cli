b836c67759d38d7be5fc2bbad9c3c288e8968a0d
lyfsn <dev.wangyu@proton.me>
2023-12-08 15:18:25 +0800

    up

--- run.new.sh
+++ run.new.sh
@@ -1,14 +1,21 @@
 # ./deposit.sh install  
 
+mkdir -p validator_keys
 
-# ./deposit.sh new-mnemonic    
+./deposit.sh \
+  --language=English \
+  --non_interactive \
+  new-mnemonic \
+  --num_validators=1 \
+  --mnemonic_language=english \
+  --chain=holesky \
+  --folder=./validator_keys \
+  --execution_address=0x95382F81598A3c9b4E8C5549B9Ea9ad24A734b13
+
 
 # marine fox season boat flee bid execute involve tree agree sick fault kind during quit monitor guard impulse half dawn drill drastic salon faith
-
-
-
-./deposit.sh new-mnemonic --num_validators=1 --mnemonic_language=english --chain=holesky 
-
-
 # rebuild envelope bean express sphere luggage carry expose name voyage limit parade curtain walnut inquiry link crystal marriage define angle tennis then match cram
 # peasant loyal smile patrol absorb regret chunk abstract hospital steak almost glad student argue myth margin end under wire badge patch rifle warrior wrist
+# process glide march maid property bread meadow print kidney artwork pretty creek atom first parent useless vacant venue kick great air exact edit van
+# sudden traffic balcony security wife burger document swing travel soul brave burst honey trust vessel true aim obvious gift jar advance public ill equip
+
