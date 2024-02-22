ae989d87c69cd7466ea9d47779f6dcd6ed909548
lyfsn <dev.wangyu@proton.me>
2024-02-22 21:50:26 +0800

    up

--- run.new.sh
+++ run.new.sh
@@ -12,3 +12,4 @@ mkdir -p validator_keys_new
   --folder=./validator_keys_new \
   --execution_address=0x6D39C4E60dEf1DfC6d09A8FdB5D075e85F0e5F8d
 
+earn detect toy faint ranch wasp ensure cash receive eye found odor surprise moral fade private believe glad portion solve piece cinnamon hundred damage
